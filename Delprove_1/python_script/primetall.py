893239384dcc3a469b918ea946e2c6778b09b5a2
Alward Munir Jarkin <alwardmj@uia.no>
2025-09-18 10:28:25 +0200

    rules and scripts

---
+++ Delprove_1/python_script/primetall.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(np.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+print(prime_factors(315))
+print(is_prime(29))
+print(is_prime(30))
